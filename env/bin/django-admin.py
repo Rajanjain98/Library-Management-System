413cc164b045efc15b5340a560a743cdff5f0e28
Rajanjain98 <rjain76@asu.edu>
2024-03-28 20:17:49 -0700

    Final Commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rajanjain/Downloads/Django-librarymanagement-main/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
